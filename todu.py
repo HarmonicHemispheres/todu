9d927ca29d3f69d92b0f2cab7113dd31dc850ca0
HarmonicHemispheres <themusicman@q.com>
2017-03-18 14:29:31 -0700

    fixed printing to be cleaner

--- todu.py
+++ todu.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-import datetime as dt
-
+from time import gmtime, strftime
+from operator import itemgetter
 
 # ========== GLOBALS FUNCS  ========== 
 
@@ -10,11 +10,11 @@ def tasklist(opt):
     if (opt == "r"):
         return open("data.txt", "r").read() 
     if (opt == "w"):
-        return open("data.txt", "w") 
+        return open("data.txt", "w+") 
 
 def main(arg=sys.argv):
-    if(len(arg) == 1):
-        show_task()
+    if(len(arg) == 1 or "-" in arg[1]):
+        show_task(arg[1:])
         return
     if (arg[1] == "add"):
         add_task(arg[2], arg[3])
@@ -23,6 +23,8 @@ def main(arg=sys.argv):
         clear()
     elif (arg[1] == "rm"):
         rm_task(arg[2]) 
+    elif (arg[1] == "show"):
+        show_task(arg[1:])
 
 # ========== FUNCTIONS ========== 
 def today():
@@ -36,23 +38,38 @@ def wl(L):
     for i in L:
         TASKS.write(i + "\n")
 
-def show_task():
 
+# todays date = strftime("%Y-%m-%d", gmtime()) 
+def show_task(cmdsList=[]):
+    sh_id = 1
+    sh_ordr = 0
+    for i in cmdsList:
+        if (i == "-id"):
+            sh_id = 1
+        if (i == "-ordr"):
+            sh_ordr = 1
     tasks = tasklist("r")
-    tl = tasks.splitlines()
-    sorted(tl)
-    sp = max(tl, key=len)
-    print(sp)
-    sp = len(sp.split("--")[1])
+    tl = [i.split("--") for i in tasks.splitlines()]
+    sp = 0
     for i in tl:
-        t = i.split("--")
-        name = t[1]
-        date = t[2]
-        print(name + " "*sp + date )
+        if (len(i[1])>sp):
+            sp = len(i[1])
+    if (sh_ordr):
+        tl.sort(key=itemgetter(2))
+    for i in tl:
+        output = ""
+        curlen = len(i[1])
+        if(sh_id):
+            output += i[0] + " "
+            ID = i[0]
+        output += i[1] + " "*(sp - curlen + 3) + i[2] + " "   # name + date
+        
+        print(output)
 
 def gen_key():
-    # generates a id for the new task based 
+    # generates an id for the new task based 
     # on the first open slot.
+
     tasks = tasklist("r")
     tl = tasks.splitlines()
     index = 0
@@ -66,7 +83,7 @@ def rm_task(ID):
     TASKS = tasklist("r") 
     tl = TASKS.splitlines()
     for i in tl:
-        if (int(i.split()[0]) == int(ID) ):
+        if (int(i.split("--")[0]) == int(ID) ):
             tl.remove(i)
             break
     wl(tl) 
