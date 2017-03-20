8799a30f0094ea49e1d49babe465d423554720ba
HarmonicHemispheres <themusicman@q.com>
2017-03-19 23:02:40 -0700

    added initial sub task framework

--- todu.py
+++ todu.py
@@ -17,6 +17,7 @@ sh_ordr = 1
 sh_thisweek = 0
 sh_lft = 0
 sh_fl = 1
+sh_sub = 0
 
 # ========== GLOBALS FUNCS  ========== 
 
@@ -31,7 +32,7 @@ def main(arg=sys.argv):
         show_task(arg[1:])
         return
     if (arg[1] == "add"):
-        add_task(arg[2], arg[3])
+        add_task(arg[2], arg[3], arg[3:])
         show_task()
     elif (arg[1] == "flag"):
        add_flag(arg[1:]) 
@@ -48,7 +49,7 @@ def main(arg=sys.argv):
 
 # ========== FUNCTIONS ========== 
 
-"""
+"""=================================================
 This function prints the contents of todu_help.txt
 to stdout. this provides a useful function for seeing
 what commands are available for todu.
@@ -57,12 +58,13 @@ def show_funcs(help_path):
     output = open(help_path, "r").read()
     for i in output.splitlines():
         print(i)
-"""
+"""=================================================
 This function returns the date of today, or if "wk"
 then it returns the date of 7 days from today.
 """
 def today(wk=0):
     today = strftime("%Y-%m-%d", gmtime())
+    print(today)
     if (wk):
         today = today.split("-")
         today[2] = str( int(today[2]) + 7 )
@@ -75,7 +77,7 @@ def today(wk=0):
 def clear():
     tasks = tasklist("w")
 
-"""
+"""==================================================
 This function takes the list of lines of tasks and
 writes them to todu_data.txt
 """
@@ -93,7 +95,7 @@ def time_left(date):
     # dtm.datetime.strptime(date, '%Y-%m-%d')
     return str(dlta.days + 1)
 
-"""
+""" ==================================================
 This function prints the contents of todu_data.txt
 to stdout. extra parameters may be passed to this
 function in order to print out specific information.
@@ -105,6 +107,7 @@ def show_task(cmdsList=[]):
     global sh_thisweek
     global sh_lft
     global sh_fl 
+    global sh_sub
     for i in cmdsList:
         if (i == "-id"):
             sh_id = (sh_id+1)%2
@@ -116,6 +119,8 @@ def show_task(cmdsList=[]):
             sh_lft = (sh_lft+1)%2
         elif (i == "-fl"):
             sh_fl = (sh_fl+1)%2
+        elif (i == "-sub"):
+            sh_sub = (sh_sub+1)%2
 
     tasks = tasklist("r")
     tl = [i.split("--") for i in tasks.splitlines()]
@@ -132,13 +137,18 @@ def show_task(cmdsList=[]):
     for i in tl:
         # check if this line should be printed
         prt = 1
+        output = ""
         if (sh_thisweek):
             onewk = today(wk=1)
             if (i[2] > onewk):
                # print("---->", i[2], "---->", onewk)
                 prt = 0
+        if (not sh_sub and "." in i[0]):
+            prt = 0
+        elif ("." in i[0]):
+            output += "---> "
+            
         if (prt):
-            output = ""
             curlen = len(i[0])
             if(sh_id):
                 output += i[0] + " "*(sp_id - curlen + 2)
@@ -156,15 +166,26 @@ def show_task(cmdsList=[]):
 Generates an id for the new task based 
 on the first open slot.
 """
-def gen_key():
+def gen_key(sub="na"):
     tasks = tasklist("r")
     tl = tasks.splitlines()
     index = 0
-    for ln in tl:
-        if (int(ln.split("--")[0]) != index):
-            return index
-        index += 1
-    return index
+    if (sub != "na"):
+        sub_in = 0
+        for ln in tl:
+            if (int(ln.split("--")[0].split(".")[0]) < int(sub)):
+                index += 1
+            elif (ln.split("--")[0].split(".")[0] == sub and len(ln.split("--")[0].split(".")) > 1):
+                if (int(ln.split("--")[0].split(".")[1]) != sub_in):
+                    return float(str(index) + "." + str(sub_in)) 
+                sub_in += 1
+        return float(str(index) + "." + str(sub_in)) 
+    else:
+        for ln in tl:
+            if (int(ln.split("--")[0]) != index):
+                return index
+            index += 1
+        return index
 
 def rm_task(ID, arg):
     rm_fl = 0
@@ -175,7 +196,7 @@ def rm_task(ID, arg):
     TASKS = tasklist("r") 
     tl = TASKS.splitlines()
     for i in range(len(tl)):
-        if (int(tl[i].split("--")[0]) == int(ID) ):
+        if (tl[i].split("--")[0] == ID ):
             if (rm_fl):
                 newline = tl[i].split("--")
                 newline[3] = " "
@@ -186,18 +207,21 @@ def rm_task(ID, arg):
             break
     wl(tl) 
 
-def add_task(item, date):
+def add_task(item, date, argL):
+    sub_id = ""
+    for i in argL:
+        if ("-sub" in i):
+            sub_id = i[4:]
+
     TASKS = tasklist("r") 
     tl = TASKS.splitlines()
-
-    if (len(sys.argv) >= 5):
-        group = sys.argv[4]
+    if (sub_id != ""):
+        ID = gen_key(sub=sub_id)
     else:
-        group = " "
-    ID = gen_key()
-    tl.append(str(ID) + "--"  + item + "--" + date + "--" + group + "--")    
+        ID = gen_key()
+    tl.append(str(ID) + "--"  + item + "--" + date + "-- --")    
     if (len(tl) > 1):
-        tl = sorted(tl, key=lambda x: int(x.split("--")[0]))
+        tl = sorted(tl, key=lambda x: float(x.split("--")[0]))
     wl(tl)   
 
 def add_flag(arg):
